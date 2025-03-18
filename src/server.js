17dd7f75bcadb876ea0890b44705ffaae7933736
pradeepkaliyawansha <pradeepkaliyawansha123@gmail.com>
2025-03-18 19:37:35 +0530

    create node server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
